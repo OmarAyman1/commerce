4ea15fde4846f3a386522b5e5de90aafd7205010
OmarAyman1 <omar1ayman1@gmail.com>
2023-07-09 15:09:32 +0300

    Edit categories

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -8,6 +8,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 
 class CategoryController extends Controller
 {
@@ -37,7 +38,7 @@ class CategoryController extends Controller
 
             $category->image = $fileName;
         }
-        
+
 
         $category->meta_title = $validatedData['meta_title'];
         $category->meta_keyword = $validatedData['meta_keyword'];
@@ -47,5 +48,45 @@ class CategoryController extends Controller
 
         $category->save();
         return Redirect('admin/category')->with('message', 'category added');
+    }
+
+
+    public function edit(Category $category){
+        return view('admin.category.edit', compact('category'));
+    }
+
+    public function update(CategoryFormRequest $request, $category){
+        $category = Category::findOrFail($category);
+
+        $validatedData = $request->validated();
+        $category = new Category;
+        $category->name = $validatedData['name'];
+        $category->slug = Str::slug($validatedData['slug']);
+        $category->description = $validatedData['description'];
+
+        if($request->hasFile('image')){
+
+            $path = 'uploads/category'.$category->image;
+            if(File::exists()){
+                File::delete();
+            }
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $fileName = time().'.'.$ext;
+
+            $file->move('uploads/category/', $fileName);
+
+            $category->image = $fileName;
+        }
+
+
+        $category->meta_title = $validatedData['meta_title'];
+        $category->meta_keyword = $validatedData['meta_keyword'];
+        $category->meta_description = $validatedData['meta_description'];
+
+        $category->status = $request->status == true ? '1':'0';
+
+        $category->update();
+        return Redirect('admin/category')->with('message', 'category updated');
     }
 }
