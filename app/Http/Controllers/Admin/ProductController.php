20f5ffe5512997bc97b71e153f4170c45bce8ef5
OmarAyman1 <omar1ayman1@gmail.com>
2023-08-07 12:04:59 +0300

    products view

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Brand;
+use App\Models\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        return view('admin.products.index');
+    }
+
+    public function create(){
+        $categories = Category::all();
+        $brands = Brand::all();
+        return view('admin.products.create', compact('categories', 'brands'));
+    }
+
+    public function store(ProductFormRequest $request){
+
+    }
 }
