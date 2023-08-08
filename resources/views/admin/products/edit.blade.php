25e463c08a9d6f9d56914301295e004aaab393c3
OmarAyman1 <omar1ayman1@gmail.com>
2023-08-08 11:57:19 +0300

    update color quantity with ajax

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -49,6 +49,11 @@
                           Images
                         </button>
                       </li>
+                      <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="color-tab" data-bs-toggle="tab" data-bs-target="#color-tab-pane" type="button" role="tab" aria-controls="color-tab-pane" aria-selected="false">
+                          colors
+                        </button>
+                      </li>
                 </ul>
                   <div class="tab-content" id="myTabContent">
                     <div class="tab-pane fade border p-3 show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
@@ -149,14 +154,107 @@
                             @endif
                         </div>
                     </div>
-                  </div>
-                  <div>
-                    <button type="submit" class="btn btn-primary">Update</button>
-                  </div>
+                    <div class="tab-pane fade border p-3" id="color-tab-pane" role="tabpanel" aria-labelledby="color-tab" tabindex="0">
+                        <div class="mb-3">
+                            <h4>add product color</h4>
+                            <label>select color</label>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    @forelse ($colors as $color)
+                                    <div class="p-2 border mb-3">
+                                        <input type="checkbox" name="colors[{{$color->id}}]" value="{{$color->id}}" />{{$color->name}}
+                                        <br/>
+                                        Quantity: <input type="number" name="colorquantity[{{$color->id}}]" style="width:70px; border:1px solid ">
+                                    </div>
+                                    @empty
+                                        <div class="col-md-12">
+                                            <h1>no colors found</h1>
+                                        </div>
+                                    @endforelse
+
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-sm table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>color name</th>
+                                        <th>quantity</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($product->productColors as $prodColor)
+                                        <tr class="prod-color-tr">
+                                            <td>
+                                                @if ($prodColor->color)
+                                                    {{$prodColor->color->name}}
+                                                @else
+                                                    no color found
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <div class="input-group mb-3" style="widt:150px">
+                                                    <input type="text" value="{{$prodColor->quantity}}" class="productColorQuantity form-control form-control-sm"/>
+                                                    <button type="button" value="{{$prodColor->id}}" class="updateProductColorBtn btn btn-primary btn-sm text-white">update</button>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <button type="button" value="{{$prodColor->id}}" class="deleteProductColorBtn btn btn-danger btn-sm text-white">delete</button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary">Update</button>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
 </div>
 
+@endsection
+
+
+@section('scripts')
+    <script>
+        $(document).ready(function () {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            $(document).on('click','.updateProductColorBtn' function () {
+                var product_id = "{{$product->id}}"
+                var prod_color_id = $(this).val();
+                var qty = $(this).closest('.prod-color-tr').find('.productColorQuantity').val();
+
+                if(qty <= 0){
+                    alert('Quantity is required');
+                    return false;
+                }
+                var data = {
+                    'product_id' : product_id,
+                    'prod_color_id' : prod_color_id,
+                    'qty': qty
+                }
+
+                $.ajax({
+                    type: "POST",
+                    url: "/admin/product-color/"+prod_color_id,
+                    data: data,
+                    success: function (response) {
+                        alert(response.message)
+                    }
+                });
+            });
+        });
+    </script>
 @endsection
