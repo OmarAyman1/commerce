4bda791b6d09cc6f4e4387a6a6824d37abc2c1c1
OmarAyman1 <omar1ayman1@gmail.com>
2023-11-28 14:33:33 +0200

    added cart items view

--- app/Http/Livewire/Frontend/Cart/CartShow.php
+++ app/Http/Livewire/Frontend/Cart/CartShow.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\Frontend\Cart;
 
+use App\Models\Cart;
 use Livewire\Component;
 
 class CartShow extends Component
 {
+
+    public $cart;
     public function render()
     {
-        return view('livewire.frontend.cart.cart-show');
+        $this->cart = Cart::where('user_id', auth()->user()->id)->get();
+        return view('livewire.frontend.cart.cart-show', [
+            'cart' => $this->cart
+        ]);
     }
 }
