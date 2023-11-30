1335536d57ae028155b1b4b288acff6be2623405
OmarAyman1 <omar1ayman1@gmail.com>
2023-11-30 15:21:42 +0200

    update product quantity after checkout

--- app/Http/Livewire/Frontend/Checkout/CheckoutShow.php
+++ app/Http/Livewire/Frontend/Checkout/CheckoutShow.php
@@ -46,6 +46,12 @@ class CheckoutShow extends Component
                 'quantity' =>  $cartItem->quantity,
                 'price' =>  $cartItem->product->selling_price,
             ]);
+            if ($cartItem->prod_color_id != null) {
+                $cartItem->productColor()->where('id', $cartItem->product_color_id)->decrement('quantity', $cartItem->quantity);
+            } else {
+                $cartItem->product()->where('id', $cartItem->product_id)->decrement('quantity', $cartItem->quantity);
+            }
+
         }
 
         return $order;
@@ -72,6 +78,7 @@ class CheckoutShow extends Component
     }
 
     public function totalProductAmount(){
+        $this->totalProductAmount=0;
         $this->carts = Cart::where('user_id', auth()->user()->id)->get();
         foreach ($this->carts as $cartItem){
             $this->totalProductAmount += $cartItem->product->selling_price*$cartItem->quantity;
